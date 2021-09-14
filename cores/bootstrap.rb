fa3ac3de8749a40373d5d59626423eda72249503
Mark24 <mark.zhangyoung@qq.com>
2021-09-14 16:32:26 +0800

    update: bootstrap

--- cores/bootstrap.rb
+++ cores/bootstrap.rb
@@ -1,11 +1,12 @@
-# Add work directory in $LOAD_PATH
-$LOAD_PATH.unshift(__dir__) unless $LOAD_PATH.include?(__dir__)
-
 # Project directory
 if !$PROJECT_DIR
   # 相对自己，绝对路径
   $PROJECT_DIR = File.expand_path(File.join(__dir__, '..'))
 end
+
+# Add work directory in $LOAD_PATH
+$LOAD_PATH.unshift($PROJECT_DIR) unless $LOAD_PATH.include?($PROJECT_DIR)
+
 
 # Inject APP_ENV
 if !$APP_ENV
