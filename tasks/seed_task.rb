a71b328b509753c7c463ab149b7d415c5d592363
Mark24 <mark.zhangyoung@qq.com>
2021-09-14 17:55:54 +0800

    update:seed

--- tasks/seed_task.rb
+++ tasks/seed_task.rb
@@ -1,7 +1,14 @@
 namespace :seed do
-  desc "Seed: init data"
-  task :init do
+  desc "Seed: run all seeds"
+  task :all do
     seeds_dir = File.expand_path(File.join($PROJECT_DIR, 'seeds'))
     Dir.glob("{#{seeds_dir}}/*.rb").sort.each { |file| require file }
+  end
+
+  desc "Seed: run seed"
+  task :run, [:seed_name] do |t, args|
+    seed_name = args[:seed_name]
+    seed_file = File.expand_path(File.join($PROJECT_DIR, 'seeds', seed_name))
+    require seed_file
   end
 end
