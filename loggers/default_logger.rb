5703fcf1c17b45566cef8efcb114b376fde93e56
Mark24 <mark.zhangyoung@qq.com>
2021-10-11 22:59:03 +0800

    fix: less warning @@logger

--- loggers/default_logger.rb
+++ loggers/default_logger.rb
@@ -1,14 +1,13 @@
-require 'logger'
+require "logger"
 
 module Loggers
-
   class << self
     environment = Config.current.app_env
-    @logger = Logger.new(File.open(File.expand_path(File.join($PROJECT_DIR,"log/#{environment}.log")), 'a'))
-    @logger.level = Logger::DEBUG if environment == 'development'
+    @@logger = Logger.new(File.open(File.expand_path(File.join($PROJECT_DIR, "log/#{environment}.log")), "a"))
+    @@logger.level = Logger::DEBUG if environment == "development"
 
     def base
-      @logger
+      @@logger
     end
   end
 end
